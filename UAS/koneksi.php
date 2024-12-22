ace577cca7ba09e8084fbdc21afd1809254c1ff8
FebiLesmana <lesmanafebi168@gmail.com>
2024-12-22 10:45:38 +0700

    Delete UAS directory

--- UAS/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost","root","", "db_smpkartini");
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
--- UAS/script.js
+++
@@ -1,6 +0,0 @@
-<script>
-AOS.init({
-    duration: 1000, // Durasi animasi
-    once: true,     // Animasi hanya berjalan sekali
-});
-</script>
